398c159e315979f772dfc69fed402d2eac450f55
Yalın Öztürk <tantoony.id@gmail.com>
2024-03-08 03:13:52 +0300

    Add Suggestion and User structs and methods

---
+++ library/Suggestion.go
@@ -0,0 +1,160 @@
+package library
+
+import (
+	db "272-backend/package/database"
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Suggestion struct {
+	ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Title    string             `json:"title" bson:"title"`
+	Content  string             `json:"content" bson:"content"`
+	AuthorID string             `json:"author" bson:"author"`
+	Upvotes  []string           `json:"upvotes" bson:"upvotes"`
+	Stars    []struct {
+		UserID string `json:"userID" bson:"userID"`
+		Star   int    `json:"star" bson:"star"`
+		Date   string `json:"date" bson:"date"`
+	} `json:"stars" bson:"stars"`
+	Date   string   `json:"date" bson:"date"`
+	Tags   []string `json:"tags" bson:"tags"`
+	Status string   `json:"status" bson:"status"`
+}
+
+func (s *Suggestion) InsertToDB() error {
+	suggestion := bson.M{
+		"title":   s.Title,
+		"content": s.Content,
+		"author":  s.AuthorID,
+		"upvotes": []string{},
+		"stars": []struct {
+			UserID string `json:"userID" bson:"userID"`
+			Star   int    `json:"star" bson:"star"`
+			Date   string `json:"date" bson:"date"`
+		}{},
+		"date":   time.Now().UTC().Format(time.RFC3339),
+		"tags":   []string{},
+		"status": "pending",
+	}
+	if _, err := db.Suggestions.InsertOne(context.TODO(), suggestion); err != nil {
+		return err
+	}
+	return nil
+}
+
+func FindSuggestionByID(id string) (Suggestion, error) {
+	var suggestion Suggestion
+	objID, _ := primitive.ObjectIDFromHex(id)
+	if err := db.Suggestions.FindOne(context.TODO(), bson.M{"_id": objID}).Decode(&suggestion); err != nil {
+		return Suggestion{}, err
+	}
+	return suggestion, nil
+}
+
+func (s *Suggestion) GiveUpvote(userID string) error {
+	for _, upvote := range s.Upvotes {
+		if upvote == userID {
+			return nil
+		}
+	}
+	update := bson.M{
+		"$addToSet": bson.M{
+			"upvotes": userID,
+		},
+	}
+	query := bson.M{
+		"_id": s.ID,
+	}
+	if _, err := db.Suggestions.UpdateOne(context.TODO(), query, update); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Suggestion) GiveStar(userID string, star int64) error {
+	for index, st := range s.Stars {
+		if st.UserID == userID {
+			s.Stars[index].Star = int(star)
+			update := bson.M{
+				"$set": bson.M{
+					"stars": s.Stars,
+				},
+			}
+			query := bson.M{
+				"_id": s.ID,
+			}
+			if _, err := db.Suggestions.UpdateOne(context.TODO(), query, update); err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+	update := bson.M{
+		"$addToSet": bson.M{
+			"stars": userID,
+		},
+	}
+	query := bson.M{
+		"_id": s.ID,
+	}
+	if _, err := db.Suggestions.UpdateOne(context.TODO(), query, update); err != nil {
+		return err
+	}
+	return nil
+}
+
+type Rejection struct {
+	ID         primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Title      string             `json:"title" bson:"title"`
+	Content    string             `json:"content" bson:"content"`
+	AuthorID   string             `json:"author" bson:"author"`
+	Upvotes    int                `json:"upvotes" bson:"upvotes"`
+	Stars      int                `json:"stars" bson:"stars"`
+	Tags       []string           `json:"tags" bson:"tags"`
+	Reasons    []string           `json:"reason" bson:"reason"`
+	ExecutorID string             `json:"executor" bson:"executor"`
+	Date       string             `json:"date" bson:"date"`
+}
+
+func (s *Suggestion) Reject(reasons []string, executorID string) error {
+	var totalStars int
+	var starCount int
+	for _, star := range s.Stars {
+		totalStars += star.Star
+		starCount++
+	}
+	average := 0
+	if starCount != 0 {
+		average = totalStars / starCount
+	}
+	rejection := Rejection{
+		Title:      s.Title,
+		Content:    s.Content,
+		AuthorID:   s.AuthorID,
+		Upvotes:    len(s.Upvotes),
+		Stars:      average,
+		Tags:       s.Tags,
+		Reasons:    reasons,
+		ExecutorID: executorID,
+		Date:       time.Now().UTC().Format(time.RFC3339),
+	}
+	if _, err := db.Rejections.InsertOne(context.TODO(), rejection); err != nil {
+		return err
+	}
+	update := bson.M{
+		"$set": bson.M{
+			"status": "rejected",
+		},
+	}
+	query := bson.M{
+		"_id": s.ID,
+	}
+	if _, err := db.Suggestions.UpdateOne(context.TODO(), query, update); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ library/user.go
@@ -0,0 +1,71 @@
+package library
+
+import (
+	db "272-backend/package/database"
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Username string             `json:"username" bson:"username"`
+	Password string             `json:"password" bson:"password"`
+	UserType string             `json:"userType" bson:"userType"`
+	Roles    []string           `json:"roles" bson:"roles"`
+	Token    string             `json:"token" bson:"token"`
+}
+
+func (u *User) InsertToDB() error {
+	user := bson.M{
+		"username": u.Username,
+		"password": u.Password,
+		"userType": u.UserType,
+		"roles":    u.Roles,
+		"token":    u.Token,
+	}
+	if _, err := db.Users.InsertOne(context.TODO(), user); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *User) PutRoles(roles []string) error {
+	u.Roles = append(u.Roles, roles...)
+	update := bson.M{
+		"$set": bson.M{
+			"roles": u.Roles,
+		},
+	}
+	query := bson.M{
+		"_id": u.ID,
+	}
+	if _, err := db.Users.UpdateOne(context.TODO(), query, update); err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetUsers() ([]User, error) {
+	var users []User
+	cursor, err := db.Users.Find(context.TODO(), bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	if err := cursor.All(context.TODO(), &users); err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func GetUser() (*User, error) {
+	var user User
+	query := bson.M{
+		"username": "admin",
+	}
+	if err := db.Users.FindOne(context.TODO(), query).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
