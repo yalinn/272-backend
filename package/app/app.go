39cdded14ffd28088561a34980c41a6193346203
Orhan Yalin ozturk <orhanyalin@hotmail.com>
2024-03-15 05:41:53 +0300

    domain change implemented

--- package/app/app.go
+++ package/app/app.go
@@ -30,7 +30,7 @@ func init() {
 	App = fiber.New()
 	App.Use(
 		cors.New(cors.Config{
-			AllowOrigins: "https://www.yalin.app",
+			AllowOrigins: "https://www.yalin.app, https://probee.yalin.app",
 			AllowHeaders: "Origin, Content-Type, Accept",
 			/* AllowOriginsFunc: func(origin string) bool {
 				return os.Getenv("ENVIRONMENT") == "development"
