77badbfa31062b14d746b5ad79468171e1b67dc9
Orhan Yalin ozturk <orhanyalin@hotmail.com>
2025-01-15 21:43:14 +0300

    ...

--- routes/portal/route.go
+++ routes/portal/route.go
@@ -18,6 +18,7 @@ func init() {
 	router.Get("/curriculum", getCurriculum)
 	router.Post("/curriculum", postCurriculum)
 	router.Post("/survey", postSurvey)
+	router.Post("/surveys", postSurveys)
 }
 
 // getCurriculum godoc
@@ -154,6 +155,68 @@ func postSurvey(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{
 			"message": "Failed to post survey",
+			"error":   err.Error(),
+		})
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Failed to read response body",
+			"error":   err.Error(),
+		})
+	}
+	var info GetResponse
+	err = json.Unmarshal(body, &info)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(info)
+}
+
+// postSurveys godoc
+// @Summary Post surveys
+// @Description Post surveys
+// @Tags portal
+// @Accept json
+// @Produce json
+// @Security Bearer
+// @Param body body BSLparams true "Body"
+// @Success 200 {object} GetResponse
+// @Router /portal/surveys [post]
+func postSurveys(c *fiber.Ctx) error {
+	data := new(BSLparams)
+	if err := c.BodyParser(data); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Invalid request",
+		})
+	}
+	user := c.Locals("user")
+	if user == nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "You are not logged in",
+		})
+	}
+	claims := user.(*jwt.Token).Claims.(jwt.MapClaims)
+	userID := claims["username"].(string)
+	if userID == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Authentication token is invalid or expired",
+		})
+	}
+	postData := map[string]interface{}{
+		"username": userID,
+		"password": data.Password,
+	}
+	jsonData, err := json.Marshal(postData)
+	if err != nil {
+		return err
+	}
+	response, err := http.Post(config.BSL_URI+"/surveys", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Failed to post surveys",
 			"error":   err.Error(),
 		})
 	}
